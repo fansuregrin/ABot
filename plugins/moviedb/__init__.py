b44af84b369af95a37dfab2690381799cecc6f97
fansuregrin <1134366185@qq.com>
2022-07-20 19:38:20 +0800

    update plugin moviedb

--- plugins/moviedb/__init__.py
+++ plugins/moviedb/__init__.py
@@ -1,12 +1,17 @@
+from ast import arg
 from bot_argparse import ArgumentParser
 from command import register_cmd
 from config import start_marks
 from .moviedb import (
     search_multi,
-    search_movie_proper,
-    search_movie_implicit,
-    search_tv_proper,
-    search_tv_implicit
+    search_movie_maybe,
+    search_movie,
+    fetch_movie,
+    search_tv_maybe,
+    search_tv,
+    fetch_tv,
+    search_person,
+    trending
 )
 
 
@@ -17,69 +22,108 @@ def cmd_qtm(args):
     parser = args['parser']
     if args['help']:
         return  '{}\n'\
-                '用法: {} [-h] <s,m> ...\n'\
+                '用法: {} [-h] <s,m,t,d> ...\n'\
                 '选项:\n'\
                 ' -h, --help 获取帮助信息\n'\
                 '子命令:\n'\
-                's 通用搜索\n'\
+                's 聚合搜索\n'\
                 't 搜索电视剧\n'\
-                'm 搜索电影'.format(
+                'm 搜索电影\n'\
+                'd 热门趋势'.format(
                     parser.description,
                     parser.prog
                 )
 
 def sub_cmd_s(args):
     args = vars(args)
-    help_str = ('用法: qtm s [-h] [-p PAGE] [要搜索的内容]\n'
+    help_str = ('用法: qtm s [-hmte] [-p PAGE] [-l LANG] <搜索内容>\n'
                 '选项:\n'
                 '-h, --help 获取帮助信息\n'
+                '-m, --movie  指明搜索电影\n'
+                '-t, --tv 指明搜索电视剧\n'
+                '-e, --person 指明搜索人物\n'
                 '-p PAGE, --page PAGE  显示搜索结果的第几页，默认为第1页\n'
-                '例如: qtm s 哈利波特 -p2')
+                '-l LANG, --language LANG 结果返回的语言\n'
+                '例如: qtm s 哈利波特 -p2 -m')
     if args['help'] or not args['query']:
         return help_str
-    query = args['query']
+    query = ' '.join(args['query'])
     page = args['page']
-    return search_multi(query, page)
+    language = args['language']
+    if args['movie']:
+        return search_movie(query, page, language)
+    elif args['tv']:
+        return search_tv(query, page, language)
+    elif args['person']:
+        return search_person(query, page, language)
+    else:
+        return search_multi(query, page, language)
 
 def sub_cmd_m(args):
     args = vars(args)
     
     parser = args['parser']
-    if args['help'] or not args['query']:
-        return  '用法: {} [-h] [-i] [-p PAGE] [要查询的电影]\n'\
+    help_str = '用法: {} [-h] [-l LANG] [-i ID] <电影名>\n'\
                 '选项:\n'\
                 '-h, --help 获取帮助信息\n'\
-                '-i, --implicit 开启模糊搜索\n'\
-                '-p PAGE, --page PAGE  显示搜索结果的第几页\n'.format(
+                '-l LANG, --language LANG 指明结果返回的语言\n'\
+                '-i ID, --id ID 指明电影id来查询\n'\
+                '例如: qtm m 星际穿越'.format(
                     parser.prog
                 )
+    if args['help']:
+        return help_str
     
-    query = args['query']
-    page = args['page']
-    if args['implicit']:
-        return search_movie_implicit(query, page)
+    query = ' '.join(args['query']).strip()
+    language = args['language']
+    id = args['id']
+    if id:
+        return fetch_movie(id, language)
+    elif query:
+        return search_movie_maybe(query, language)
     else:
-        return search_movie_proper(query)
+        return help_str
 
 def sub_cmd_t(args):
     args = vars(args)
     
     parser = args['parser']
-    if args['help'] or not args['query']:
-        return  '用法: {} [-h] [-i] [-p PAGE] [要查询的电视剧]\n'\
+    help_str = '用法: {} [-h] [-i ID] [-l LANG] <剧集名>\n'\
                 '选项:\n'\
                 '-h, --help 获取帮助信息\n'\
-                '-i, --implicit 开启模糊搜索\n'\
-                '-p PAGE, --page PAGE  显示搜索结果的第几页\n'.format(
-                    parser.prog
+                '-l LANG, --language LANG 指明结果返回的语言\n'\
+                '-i ID, --id ID 指明电视剧id来查询\n'.format(
+                parser.prog
                 )
     
-    query = args['query']
-    page = args['page']
-    if args['implicit']:
-        return search_tv_implicit(query, page)
+    if args['help']:
+        return help_str
+    
+    query = ' '.join(args['query']).strip()
+    language = args['language']
+    id = args['id']
+    if id:
+        return fetch_tv(id, language)
+    elif query:
+        return search_tv_maybe(query, language)
     else:
-        return search_tv_proper(query)
+        return help_str
+
+def sub_cmd_d(args):
+    args = vars(args)
+
+    parser = args['parser']
+    help_str = '用法: {} [-h] [-t TYPE] [-l LANG] [-w WINDOW]\n'\
+                '选项:\n'\
+                '-h, --help 获取帮助信息\n'\
+                '-t, --type 类型: movie, tv, person, all\n'\
+                '-l LANG, --language LANG 结果显示的语言\n'\
+                '-w, --window 热门时间: day, week\n'.format(parser.prog)
+
+    if args['help']:
+        return help_str
+    else:
+        return trending(args['type'], args['window'], args['language'])
 
 
 def qtm_parse_args(args):
@@ -90,24 +134,36 @@ def qtm_parse_args(args):
     sub_parsers = parser.add_subparsers(title='子命令')
 
     parser_s = sub_parsers.add_parser('s', help='通用搜索', add_help=False)
-    parser_s.add_argument('query', type=str, help='要搜索的内容', nargs='?', default='')
+    parser_s.add_argument('query', type=str, help='要搜索的内容', nargs='*')
     parser_s.add_argument('-p', '--page', type=int, help='显示搜索结果的第几页', default=1, dest='page')
+    parser_s.add_argument('-l', '--language', type=str, help='结果返回的语言', default='zh', dest='language')
     parser_s.add_argument('-h', '--help', action='store_true')
+    search_type_group = parser_s.add_mutually_exclusive_group()
+    search_type_group.add_argument('-m', '--movie', action='store_true')
+    search_type_group.add_argument('-t', '--tv', action='store_true')
+    search_type_group.add_argument('-e', '--person', action='store_true')
     parser_s.set_defaults(func=sub_cmd_s, parser=parser_s)
     
     parser_m = sub_parsers.add_parser('m', help='搜索电影', add_help=False)
-    parser_m.add_argument('query', type=str, help='要查询的电影', default='', nargs='?')
-    parser_m.add_argument('-p', '--page', type=int, help='显示搜索结果的第几页', default=1, dest='page')
+    parser_m.add_argument('query', type=str, help='要查询的电影名称', nargs='*')
+    parser_m.add_argument('-i', '--id', type=int, default=0, dest='id')
+    parser_m.add_argument('-l', '--language', type=str, default='zh', dest='language')
     parser_m.add_argument('-h', '--help', action='store_true')
-    parser_m.add_argument('-i', '--implicit', action='store_true', help='开启模糊搜索', dest='implicit')
     parser_m.set_defaults(func=sub_cmd_m, parser=parser_m)
 
     parser_t = sub_parsers.add_parser('t', help='搜索电视剧', add_help=False)
-    parser_t.add_argument('query', type=str, help='要查询的电视剧', default='', nargs='?')
-    parser_t.add_argument('-p', '--page', type=int, help='显示搜索结果的第几页', default=1, dest='page')
+    parser_t.add_argument('query', type=str, help='要查询的电影名称', nargs='*')
+    parser_t.add_argument('-i', '--id', type=int, default=0, dest='id')
+    parser_t.add_argument('-l', '--language', type=str, default='zh', dest='language')
     parser_t.add_argument('-h', '--help', action='store_true')
-    parser_t.add_argument('-i', '--implicit', action='store_true', help='开启模糊搜索', dest='implicit')
-    parser_t.set_defaults(func=sub_cmd_t, parser=parser_t)
+    parser_t.set_defaults(func=sub_cmd_t, parser=parser_m)
+
+    parser_d = sub_parsers.add_parser('d', help='趋势', add_help=False)
+    parser_d.add_argument('-t', '--type', type=str, choices=['movie', 'tv', 'person', 'all'], default='all')
+    parser_d.add_argument('-w', '--window', type=str, choices=['day', 'week'], default='day')
+    parser_d.add_argument('-l', '--language', type=str, default='zh')
+    parser_d.add_argument('-h', '--help', action='store_true')
+    parser_d.set_defaults(func=sub_cmd_d, parser=parser_d)
 
     if not args:
         return '建议您使用"qtm --help"或"qtm -h"看看帮助手册，但别忘了以{}开头！'.format('或'.join(start_marks))
--- plugins/moviedb/moviedb.py
+++ plugins/moviedb/moviedb.py
@@ -8,57 +8,24 @@ base_url = 'https://api.themoviedb.org/3'
 img_base_url = 'https://image.tmdb.org/t/p/original'
 
 
-def search_movie_proper(query):
+def search_movie_maybe(query, language):
     url = base_url + '/search/movie'
     params = {
         'api_key': tmdb_api_key,
         'include_adult': 'false',
-        'language': 'zh',
+        'language': language,
         'page': 1,
         'query': query
     }
     resp = requests.get(url, params=params).json()
     if 'results' in resp.keys():
-        if resp['results']:
-            movie = resp['results'][0]
+        movie_list = resp['results']
+        if movie_list:
             try:
-                movie_info = '影名: {}\n原名: {}\n评分: {}分\n简介:{}\n上映日期: {}'.format(
-                    movie['title'],
-                    movie['original_title'],
-                    movie['vote_average'],
-                    movie['overview'],
-                    movie['release_date'],
-                )
-
-                # generate image with informations of a movie
-                target_folder = f"{images_dir}/movies/{movie['id']}"
-                movie_info_img_save_path = f"{target_folder}/{movie['title']}_info"
-                if not os.path.exists(target_folder):
-                    os.makedirs(target_folder)
-                gen_imgs = gen_img_with_text( 
-                    movie_info, 
-                    'plugins/moviedb/fonts/wqy-microhei.ttc', 
-                    movie_info_img_save_path,
-                    bg_folder = 'plugins/moviedb/bg'
-                )
-
-                # retrive and save tvshow's poster
-                movie_poster_save_path = f"{target_folder}{movie['poster_path']}"
-                if not os.path.exists(movie_poster_save_path):
-                    poster_data = requests.get(img_base_url + movie['poster_path']).content
-                    with open(movie_poster_save_path, 'wb') as fp:
-                        fp.write(poster_data)
-                
-                outcome = ''
-                for gen_img in gen_imgs:
-                    outcome = outcome + '[CQ:image,file={}]'.format(
-                        gen_img.replace(f'{images_dir}/', '')
-                    )
-                outcome = '{}\n[CQ:image,file={}]'.format(
-                    outcome,
-                    f"movies/{movie['id']}{movie['poster_path']}"
-                )
-                
+                movie = max(movie_list, key=lambda x: x['popularity'])
+                movie_id = movie['id']
+                outcome = fetch_movie(movie_id, language)
+                    
             except Exception as err:
                 outcome = str(err)
         else:
@@ -68,12 +35,58 @@ def search_movie_proper(query):
     
     return outcome
 
-def search_movie_implicit(query, page):
+def fetch_movie(id, language):
+    url = base_url + f'/movie/{id}?api_key={tmdb_api_key}&language={language}'
+    resp = requests.get(url).json()
+    movie_info = '片名: {}\n原产地片名: {}\n时长: {}h{}min\n评分: {}\n类型: {}\n简介: {}\n上映日期: {}\nimdb_id: {}\n预算: ${}\n票房: ${}'.format(
+        resp.get('title'),
+        resp.get('original_title'),
+        resp.get('runtime', 0)//60, resp.get('runtime', 0)%60,
+        resp.get('vote_average', '无'),
+        ' '.join(genre.get('name', '') for genre in resp.get('genres')),
+        resp.get('overview', '无'),
+        resp.get('release_date'),
+        resp.get('imdb_id'),
+        resp.get('budget', 0),
+        resp.get('revenue', 0)
+    )
+    poster_path = resp.get('poster_path')
+
+    # generate image with informations of a movie
+    target_folder = f"{images_dir}/movies/{id}"
+    movie_info_img_save_path = f"{target_folder}/movie_info"
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    gen_imgs = gen_img_with_text( 
+        movie_info, 
+        'plugins/moviedb/fonts/wqy-microhei.ttc', 
+        movie_info_img_save_path,
+        bg_folder = 'plugins/moviedb/bg'
+    )
+
+    # retrive and save movie's poster
+    movie_poster_save_path = f"{target_folder}/{poster_path}"
+    if not os.path.exists(movie_poster_save_path):
+        poster_data = requests.get(img_base_url + poster_path).content
+        with open(movie_poster_save_path, 'wb') as fp:
+            fp.write(poster_data)
+
+    # generate CQ code
+    gen_imgs.append(movie_poster_save_path)
+    outcome = ''
+    for gen_img in gen_imgs:
+        outcome = outcome + '[CQ:image,file={}]'.format(
+            gen_img.replace(f'{images_dir}/', '')
+        )
+
+    return outcome
+
+def search_movie(query, page, language):
     url = base_url + '/search/movie'
     params = {
         'api_key': tmdb_api_key,
         'include_adult': 'false',
-        'language': 'zh',
+        'language': language,
         'page': page,
         'query': query
     }
@@ -84,12 +97,13 @@ def search_movie_implicit(query, page):
             try:
                 movies_info = []
                 for movie in movies:
+                    unspec = '暂无'
                     movie_info = '影名: {} 原名: {} 评分: {}分 id:{} 上映日期:{}'.format(
-                        movie['title'],
-                        movie['original_title'],
-                        movie['vote_average'] if 'vote_average' in movie.keys() else '暂无',
-                        movie['id'],
-                        movie['release_date']
+                        movie.get('title', unspec),
+                        movie.get('original_title', unspec),
+                        movie.get('vote_average', unspec),
+                        movie.get('id', unspec),
+                        movie.get('release_date', unspec)
                     )
                     movies_info.append(movie_info)
 
@@ -119,57 +133,23 @@ def search_movie_implicit(query, page):
 
     return outcome
 
-def search_tv_proper(query):
+def search_tv_maybe(query, language):
     url = base_url + '/search/tv'
     params = {
         'api_key': tmdb_api_key,
         'include_adult': 'false',
-        'language': 'zh',
+        'language': language,
         'page': 1,
         'query': query
     }
     resp = requests.get(url, params=params).json()
     if 'results' in resp.keys():
-        tvs = resp['results']
-        if tvs:
+        tv_list = resp['results']
+        if tv_list:
             try:    
-                tv = tvs[0]
-                tv_info = '剧名: {}\n原名: {}\n评分: {}分 \n简介:{}\n上映日期:{}'.format(
-                    tv['name'],
-                    tv['original_name'],
-                    tv['vote_average'] if 'vote_average' in tv.keys() else '暂无',
-                    tv['overview'],
-                    tv['first_air_date']
-                )
-                
-                # generate image with informations of a tvshow
-                target_folder = f"{images_dir}/tvs"
-                results_info_img_save_path = f"{target_folder}/outcome"
-                if not os.path.exists(target_folder):
-                    os.makedirs(target_folder)
-                gen_imgs = gen_img_with_text(
-                    tv_info,
-                    'plugins/moviedb/fonts/wqy-microhei.ttc',
-                    results_info_img_save_path,
-                    bg_folder = 'plugins/moviedb/bg'
-                )
-
-                # retrive and save tvshow's poster
-                tv_poster_save_path = f"{target_folder}{tv['poster_path']}"
-                if not os.path.exists(tv_poster_save_path):
-                    poster_data = requests.get(img_base_url + tv['poster_path']).content
-                    with open(tv_poster_save_path, 'wb') as fp:
-                        fp.write(poster_data)
-                
-                outcome = ''
-                for gen_img in gen_imgs:
-                    outcome = outcome + '[CQ:image,file={}]'.format(
-                        gen_img.replace(f'{images_dir}/', '')
-                    )
-                outcome = '{}\n[CQ:image,file={}]'.format(
-                    outcome,
-                    f"movies/{tv['id']}{tv['poster_path']}"
-                )
+                tv = max(tv_list, key=lambda x: x['popularity'])
+                tv_id = tv['id']
+                outcome = fetch_tv(tv_id, language)
                 
             except Exception as err:
                 outcome = str(err)
@@ -180,12 +160,63 @@ def search_tv_proper(query):
 
     return outcome
 
-def search_tv_implicit(query, page):
+def fetch_tv(id, language):
+    url = base_url + f'/tv/{id}?api_key={tmdb_api_key}&language={language}'
+    resp = requests.get(url).json()
+    tv_info = '剧名: {}\n原产地剧名: {}\n评分: {}/10\n已播季数: {}季\n已播集数: {}集\n简介: {}\n首播日期: {}\n最近播出日期: {}\n'\
+              '类型: {}\n播放平台: {}\n状态: {}\n创作者: {}\n{}'.format(
+        resp.get('name'),
+        resp.get('original_name'),
+        resp.get('vote_average'),
+        resp.get('number_of_seasons'),
+        resp.get('number_of_episodes'),
+        resp.get('overview'),
+        resp.get('first_air_date'),
+        resp.get('last_air_date'),
+        ' '.join(genre.get('name', '') for genre in resp.get('genres')),
+        ' '.join(network.get('name', '') for network in resp.get('networks')),
+        resp.get('status'),
+        ' & '.join(creator.get('name', '') for creator in resp.get('created_by')),
+        '\n'.join( f"({season['season_number']}) {season['name']} {season['air_date']} 共{season['episode_count']}集 {season['overview']}" 
+                    for season in resp.get('seasons') )
+    )
+    poster_path = resp.get('poster_path')
+
+    # generate image with informations of a tv
+    target_folder = f"{images_dir}/tvs/{id}"
+    tv_info_img_save_path = f"{target_folder}/tv_info"
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    gen_imgs = gen_img_with_text( 
+        tv_info, 
+        'plugins/moviedb/fonts/wqy-microhei.ttc', 
+        tv_info_img_save_path,
+        bg_folder = 'plugins/moviedb/bg'
+    )
+
+    # retrive and save tv's poster
+    tv_poster_save_path = f"{target_folder}/{poster_path}"
+    if not os.path.exists(tv_poster_save_path):
+        poster_data = requests.get(img_base_url + poster_path).content
+        with open(tv_poster_save_path, 'wb') as fp:
+            fp.write(poster_data)
+
+    # generate CQ code
+    gen_imgs.append(tv_poster_save_path)
+    outcome = ''
+    for gen_img in gen_imgs:
+        outcome = outcome + '[CQ:image,file={}]'.format(
+            gen_img.replace(f'{images_dir}/', '')
+        )
+
+    return outcome
+
+def search_tv(query, page, language):
     url = base_url + '/search/tv'
     params = {
         'api_key': tmdb_api_key,
         'include_adult': 'false',
-        'language': 'zh',
+        'language': language,
         'page': page,
         'query': query
     }
@@ -196,12 +227,13 @@ def search_tv_implicit(query, page):
             try:
                 tvs_info = []
                 for tv in tvs:
-                    tv_info = '剧名: {} 原名: {} 评分: {}分 id:{} 上映日期:{}'.format(
-                        tv['name'],
-                        tv['original_name'],
-                        tv['vote_average'] if 'vote_average' in tv.keys() else '暂无',
-                        tv['id'],
-                        tv['first_air_date']
+                    unspec = '暂无'
+                    tv_info = '剧名: {} 原名: {} 评分: {}分 id:{} 首播日期:{}'.format(
+                        tv.get('name', unspec),
+                        tv.get('original_name', unspec),
+                        tv.get('vote_average', unspec),
+                        tv.get('id', unspec),
+                        tv.get('first_air_date', unspec)
                     )
                     tvs_info.append(tv_info)
 
@@ -231,12 +263,62 @@ def search_tv_implicit(query, page):
 
     return outcome
 
-def search_multi(query, page):
+def search_person(query, page, language):
+    url = base_url + '/search/person'
+    params = {
+        'api_key': tmdb_api_key,
+        'include_adult': 'false',
+        'language': language,
+        'page': page,
+        'query': query
+    }
+    resp = requests.get(url, params=params).json()
+    if 'results' in resp.keys():
+        persons = resp['results']
+        if persons:
+            try:
+                persons_info = []
+                for person in persons:
+                    unspec = '暂无'
+                    person_info = '{} id: {} 知名领域: {}'.format(
+                        person.get('name', unspec),
+                        person.get('id', unspec),
+                        person.get('known_for_department', unspec)
+                    )
+                    persons_info.append(person_info)
+
+                target_folder = f"{images_dir}/tmdb_persons"
+                results_info_img_save_path = f"{target_folder}/outcome"
+                if not os.path.exists(target_folder):
+                    os.makedirs(target_folder)
+                gen_imgs = gen_img_with_text(
+                    '\n'.join(persons_info),
+                    'plugins/moviedb/fonts/wqy-microhei.ttc',
+                    results_info_img_save_path,
+                    bg_folder = 'plugins/moviedb/bg'
+                )
+
+                outcome = ''
+                for gen_img in gen_imgs:
+                    outcome = outcome + '[CQ:image,file={}]'.format(
+                        gen_img.replace(f'{images_dir}/', '')
+                    )
+                
+            except Exception as err:
+                outcome = str(err)
+        else:
+            outcome = f'未找到第{page}页与”{query}“相关的人物！'
+    else:
+        outcome = '查找出错啦！'
+
+    return outcome
+
+def search_multi(query, page, language):
     url = base_url + '/search/multi'
     params = {
         'api_key': tmdb_api_key,
         'include_adult': 'false',
-        'language': 'zh',
+        'language': language,
         'page': page,
         'query': query
     }
@@ -247,24 +329,26 @@ def search_multi(query, page):
             try:
                 results_info = []
                 for result in results:
-                    media_type_map = {
-                        'movie': '电影',
-                        'tv': '电视剧',
-                        'person': '个人'
-                    }
-                    if 'release_date' in result.keys():
-                        release_date = result['release_date']
-                    elif 'first_air_date' in result.keys():
-                        release_date =  result['first_air_date']
-                    else:
-                        release_date = '无'
-                    result_info = '{} {} {}分 id:{} 上映日期:{}'.format(
-                        result['title'] if result['media_type'] == 'movie' else result['name'],
-                        media_type_map[result['media_type']],
-                        result['vote_average'] if 'vote_average' in result.keys() else '暂无',
-                        result['id'],
-                        release_date
-                    )
+                    if result['media_type'] == 'movie':
+                        result_info = '{} [电影] id: {} 评分: {}/10 上映日期: {}'.format(
+                            result.get('title'),
+                            result.get('id'),
+                            result.get('vote_average', '暂无'),
+                            result.get('release_date', '暂无')
+                        )
+                    elif result['media_type'] == 'tv':
+                        result_info = '{} [剧集] id: {} 评分: {}/10 首播日期: {}'.format(
+                            result.get('name'),
+                            result.get('id'),
+                            result.get('vote_average', '暂无'),
+                            result.get('first_air_date', '暂无')
+                        )
+                    elif result['media_type'] == 'person':
+                        result_info = '{} [人物] id: {} 知名领域: {}'.format(
+                            result.get('name'),
+                            result.get('id'),
+                            result.get('known_for_department')
+                        )
                     results_info.append(result_info)
 
                 target_folder = f"{images_dir}/tmdb_multi"
@@ -290,6 +374,61 @@ def search_multi(query, page):
             outcome = f'未找到第{page}页与”{query}“相关的电视剧或电影！'
     else:
         outcome = '查找出错啦！'
+
+    return outcome
+
+
+def trending(media_type, time_window, language):
+    url = base_url + f'/trending/{media_type}/{time_window}'
+    params = {
+        'api_key': tmdb_api_key,
+        'language': language,
+        'page': 1
+    }
+    resp = requests.get(url, params=params).json()
+    media_list = resp.get('results')
+    if not media_list:
+        return '出错啦~'
+    media_list.sort(key=lambda x: x['popularity'], reverse=True)
+    
+    if media_type == 'all':
+        media_map = {
+            'movie': ('title', '电影'),
+            'tv': ('name', '剧集'),
+            'person': ('name', '人物')
+        }
+        medias_info = '\n'.join(f"{media[media_map[media['media_type']][0]]}  [{media_map[media['media_type']][1]}] id:{media['id']}" 
+                        for media in media_list)
+    elif media_type == 'movie':
+        medias_info = '\n'.join(f"{media['title']} {media['vote_average']}分 {media['release_date']} id:{media['id']}"
+                        for media in media_list)
+    elif media_type == 'tv':
+        medias_info = '\n'.join(f"{media['name']} {media['vote_average']}分 {media['first_air_date']} id:{media['id']}"
+                        for media in media_list)
+    elif media_type == 'person':
+        medias_info = '\n'.join(f"{media['name']} {media['known_for_department']} id:{media['id']}"
+                        for media in media_list)
+
+    target_folder = f'{images_dir}/tmdb_trending'
+    medias_info_img_save_path = f"{target_folder}/outcome"
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    gen_imgs = gen_img_with_text(
+        medias_info,
+        'plugins/moviedb/fonts/wqy-microhei.ttc',
+        medias_info_img_save_path,
+        bg_folder = 'plugins/moviedb/bg'
+    )
+
+    tw_map = {
+        'day': '24小时内',
+        'week': '一周内'
+    }
+    outcome = f'{tw_map[time_window]}排行榜\n'
+    for gen_img in gen_imgs:
+        outcome = outcome + '[CQ:image,file={}]'.format(
+            gen_img.replace(f'{images_dir}/', '')
+        )
 
     return outcome
 
